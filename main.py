266da111381b4f2eb59c37e03149c7db151d459b
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2022-07-16 17:42:52 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+"""The idea behind the project is to use a iterative process that will go through all possible steps that can be taken
+and will return the correct solution
+
+The game used is a game where you have to place several pieces inside a grid (see documentation for images)
+"""
