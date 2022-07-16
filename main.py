35f1300f14c7ead4f8a4fbd9d9f3d448eacf7381
wardgeronimussmets <81919921+wardgeronimussmets@users.noreply.github.com>
2022-07-16 18:08:38 +0200

    Added pieces

--- main.py
+++ main.py
@@ -3,3 +3,34 @@ and will return the correct solution
 
 The game used is a game where you have to place several pieces inside a grid (see documentation for images)
 """
+
+#definitions
+dag = [[1,1,0,0],     #green
+     [0,1,1,1,]]
+ora = [[1,0,0],
+     [1,1,1]]   #orange
+red = [[1,1,0],
+     [1,1,1]]   #red
+lib = [[1,1,1],
+     [0,0,1],
+     [0,0,1]]   #light blue
+gre = [[0,1,0],
+     [1,1,1],
+     [0,1,0]]   #grey
+pur = [[1,1,1,1]] #purple
+dab = [[0,1],
+     [0,1],
+     [0,1],
+     [1,1]] #dark blue
+bei = [[1,1,1,0],
+       [0,0,1,0]] #beige
+pin = [[1,1,0],
+       [0,1,1,],
+       [0,0,1]] #pink
+whi = [[1,0],
+       [1,1]]   #white
+yel = [[1,1],
+       [0,1],
+       [1,1]] #yellow
+lig = [[1,1],
+       [1,1]] #light green
